b54846d7cebb0a98d5ead1d07b66692fcd2d1236
chengkangzai <pycck@hotmail.com>
2019-08-27 17:40:13 +0800

    27082019

---
+++ 27082019/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$name=$_POST['user'];
+echo "Hello " .$name ;    ?>
+<br>
+<a href="index.html">back</a>
+</body>
+</html>
